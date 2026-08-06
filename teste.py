036ad83ad36867dc1daf6a3a9f342813444781af
Gabriel Matos <gabriel94749@gmail.com>
2026-08-06 13:23:14 -0300

    Atualizando Script interativo

--- teste.py
+++ teste.py
@@ -1,1 +1,8 @@
-print("Estou Aprendendo Python!")
+#Primeiro script em Python - Gabriel Matos
+print("===============================")
+print("        PROJETO GYMCODE")
+print("===============================")
+#Entrada de Dados
+nome=input("Digite seu nome: ")
+curso=input("Digite seu curso: ")
+print(f"\nOlá {nome}! Seu registro no curso de {curso} foi confirmado com sucesso.")
